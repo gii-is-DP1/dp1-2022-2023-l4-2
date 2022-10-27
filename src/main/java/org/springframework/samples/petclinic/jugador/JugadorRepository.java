dc2c6fb0c0dfc4b718c3c5d3a03bdcb78817d76b
David Reyes <davidreyesales@gmail.com>
2022-10-27 12:01:42 +0200

    Jugador controller y repository

---
+++ src/main/java/org/springframework/samples/petclinic/jugador/JugadorRepository.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.jugador;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JugadorRepository extends CrudRepository<Jugador, Long> {
+
+    List<Jugador> findAll();
+    
+}
