d0e9821fcf19544bc8c71196555e87ef1fe312b7
PabloMera6 <pabmergom@alum.us.es>
2022-11-18 12:54:16 +0100

    Participaciones service y repository Fixes #174

---
+++ src/main/java/org/springframework/samples/petclinic/partida/ParticipacionRepository.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.partida;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+@Repository
+public interface ParticipacionRepository extends CrudRepository<Participacion,Long>{
+    
+    List<Participacion> findAll();
+
+}
