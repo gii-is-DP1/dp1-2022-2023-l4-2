b092364572f44ae9d7718fdfc21c6806352ffcab
Alejandrocg024 <alecamgal1@alum.us.es>
2022-12-02 20:52:38 +0100

    Votos Tests Reporitory y Service Fixes #222

--- src/main/java/org/springframework/samples/petclinic/partida/VotoService.java
+++ src/main/java/org/springframework/samples/petclinic/partida/VotoService.java
@@ -37,10 +37,16 @@ public class VotoService {
     }
     
     @Transactional(readOnly = true)
-    public List<Voto> getVotosTurnoJugador(Partida p, Jugador j){
-        return votoRepository.findVotosTurnoJugador(j, p.getTurno(), p.getRonda(),p);
+    public List<Voto> getVotosTurnoJugador(Partida p, Jugador j) throws Exception{
+        if (p.getJugadores().contains(j)){
+            return votoRepository.findVotosTurnoJugador(j, p.getTurno(), p.getRonda(),p);
+        }
+        else {
+            throw new Exception("El usuario" + j.getUser().getUsername() + " no ha jugado la partida");
+        }
     }
 
+    @Transactional(readOnly = true)
     public Optional<Voto> getVotoById(Long votoId) {
         return votoRepository.findById(votoId.intValue());
     }
