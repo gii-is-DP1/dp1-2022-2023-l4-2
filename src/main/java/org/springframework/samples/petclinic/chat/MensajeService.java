0fc5ef054c151cf7a6d23420b8a3f40787be296c
PabloMera6 <pabmergom@alum.us.es>
2022-12-09 09:59:54 +0100

    Refactorización chat

--- src/main/java/org/springframework/samples/petclinic/chat/MensajeService.java
+++ src/main/java/org/springframework/samples/petclinic/chat/MensajeService.java
@@ -1,10 +1,10 @@
 package org.springframework.samples.petclinic.chat;
 
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.jugador.Jugador;
-import org.springframework.samples.petclinic.partida.Partida;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,6 +33,18 @@ public class MensajeService {
     @Transactional
     public void saveMensaje(Mensaje m) throws DataAccessException{
         mensajeRepo.save(m);
+    }
+
+    @Transactional(readOnly = true)
+    public Integer devuelveIdMax(){
+        Integer idMax;
+        List<Mensaje> todos = getAll();
+        if(todos.isEmpty()){
+            idMax=0;
+        }else{
+            idMax= todos.stream().max(Comparator.comparing(x -> x.getId())).map(x -> x.getId()).get();
+        }
+        return idMax;
     }
 
 }
