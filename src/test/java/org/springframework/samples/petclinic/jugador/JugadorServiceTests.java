d10f59f641805b865ab8c2b43d39ba4a6b159873
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-01-11 18:45:22 +0100

    Más test de jugadorService

--- src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/jugador/JugadorServiceTests.java
@@ -1,14 +1,18 @@
 package org.springframework.samples.petclinic.jugador;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.samples.petclinic.partida.Participacion;
 import org.springframework.samples.petclinic.partida.Partida;
+import org.springframework.samples.petclinic.partida.PartidaService;
 import org.springframework.samples.petclinic.user.User;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +29,9 @@ public class JugadorServiceTests {
     
     @Autowired
     protected JugadorService jugadorService;
+
+    @Autowired
+    protected PartidaService partidaService;
 
     @Test
     public void getJugadoresTest(){
@@ -183,4 +190,92 @@ public class JugadorServiceTests {
         jugadorService.agregarAmigo(jugador8, jugador7.getUser().getUsername());
         assertEquals(listaVacia, jugadorService.getPartidasActivasAmigos(jugador8));
     }
+
+    @Test
+    public void testGetRoles(){
+        List<RolType> roles = jugadorService.getRoles();
+        assertNotNull(roles);
+        assertFalse(roles.isEmpty());
+        assertTrue(!roles.stream()
+                .filter(x->x.getName().equals("Consul"))
+                .collect(Collectors.toList())
+                .isEmpty());
+        assertTrue(!roles.stream()
+                .filter(x->x.getName().equals("Edil"))
+                .collect(Collectors.toList())
+                .isEmpty());
+        assertTrue(!roles.stream()
+                .filter(x->x.getName().equals("Pretor"))
+                .collect(Collectors.toList())
+                .isEmpty());
+        assertTrue(!roles.stream()
+                .filter(x->x.getName().equals("Sin rol"))
+                .collect(Collectors.toList())
+                .isEmpty());
+    }
+
+    @Test
+    public void testReparteRolesRonda2(){
+        Jugador j1 = new Jugador();
+        j1.setFirstName("FEKIR");
+        j1.setLastName("Fekir");
+        j1.setId(123123);
+        User u = new User();
+        u.setEnabled(true);
+        u.setPassword("Fekircillo");
+        u.setPassword("adfsadf");
+        j1.setUser(u);
+        User u2 = new User();
+        u2.setEnabled(true);
+        u2.setPassword("Fekircillo2");
+        u2.setPassword("adfsadf2");
+        j1.setUser(u2);
+        Jugador j2 = new Jugador();
+        j2.setId(12341);
+        j2.setUser(u);
+        j2.setFirstName("Luiz");
+        j2.setLastName("Padrique");
+        Partida p = new Partida();
+        p.setTurno(1);
+        Participacion part1 = new Participacion();
+        part1.setNumConsul(1);
+        part1.setId(134);
+        Participacion part2 = new Participacion();
+        part2.setNumConsul(2);
+        part2.setId(3467);
+        p.setNumJugadores(5);
+        j1.setPartidas(List.of(p));
+        j2.setPartidas(List.of(p));
+        j1.setParticipaciones(List.of(part1));
+        j2.setParticipaciones(List.of(part2));
+        p.setParticipaciones(List.of(part1,part2));
+        p.setJugadores(List.of(j1,j2));
+        assertEquals(j1.getParticipacionEnPartida(p), part1);
+        assertEquals(j2.getParticipacionEnPartida(p), part2);
+        jugadorService.preparaRolesRonda2(p);
+        assertEquals("Consul",j1.getRol().getName());
+        assertEquals("Sin rol", j2.getRol().getName());
+    }
+
+    @Test
+    public void testAñadePartidaAJugador(){
+        Jugador j1 = jugadorService.getJugadorByUsername("Guaje");
+        Partida p1 = partidaService.getPartidaById(5).orElse(null);
+        assertFalse(j1.getPartidas().contains(p1));
+        jugadorService.añadePartidaAJugador(p1, j1);
+        assertTrue(j1.getPartidas().contains(p1));
+    }
+
+    @Test
+    public void testAñadePartidaAJugadorNegative(){ //Probamos a añadirle una partida en la que ya haya participado y comprobamos que no ocurre nada extraño
+        Jugador j1 = jugadorService.getJugadorByUsername("Guaje");
+        Partida p1 = partidaService.getPartidaById(1).orElse(null);
+        assertTrue(j1.getPartidas().contains(p1));
+        jugadorService.añadePartidaAJugador(p1, j1);
+        assertTrue(j1.getPartidas().contains(p1));
+        assertEquals(j1.getPartidas().stream().filter(x->x.equals(p1)).count(), 1.);// Compruebo que la partida solo se ha añadido una vez
+    }
+
+    
+
 }
