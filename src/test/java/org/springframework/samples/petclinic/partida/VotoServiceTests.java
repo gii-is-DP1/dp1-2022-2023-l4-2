e82a516cd00af045b6496929484fcd504fc7e552
Juan Jesús Campos Garrido <guaje3181@gmail.com>
2023-01-12 12:43:27 +0100

    Más test y correcciones

--- src/test/java/org/springframework/samples/petclinic/partida/VotoServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/partida/VotoServiceTests.java
@@ -9,12 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.jugador.Jugador;
 import org.springframework.samples.petclinic.jugador.JugadorService;
+import org.springframework.samples.petclinic.jugador.RolType;
+import org.springframework.samples.petclinic.user.User;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -31,79 +34,71 @@ public class VotoServiceTests {
         this.jugadorService = jugadorService;
     }
 
+    Jugador j= new Jugador();
+    Partida p = new Partida();
+    Voto v= new Voto();
+
+    @BeforeEach
+    public void config(){
+        p.setId(123141);
+        User u = new User();
+        u.setUsername("Pepito");
+        u.setPassword("gawdf");
+        j.setUser(u);
+        j.setFirstName("wrert");
+        j.setLastName("wrtewre");
+        RolType rt = new RolType();
+        rt.setId(123123);
+        rt.setName("Edil");
+        j.setId(13413);
+        j.setRol(rt);
+        v.setId(21512);
+        v.setElegido(false);
+        FaccionType ft = new FaccionType();
+        ft.setId(31413);
+        ft.setName("Leal");
+        v.setFaccion(ft);
+        v.setJugador(j);
+        v.setRonda(1);
+        v.setTurno(1);
+        v.setPartida(p);
+        p.setJugadores(List.of(j));
+    }
+
    
     @Test
     public void getVotosTest() throws VotoNoPermitidoException{
-        //Añadir voto
-        Voto voto = new Voto();
-        voto.setRonda(2);
-        voto.setTurno(1);
-        votoService.saveVoto(voto,null);
-
         List<Voto> votos = votoService.getVotos();
         assertNotNull(votos);
         assertFalse(votos.isEmpty());
     }
 
     @Test
-    public void getVotosRondaTurnoTest() throws VotoNoPermitidoException{
-        //Partida
-        Partida partida1 = partidaService.getPartidaById(1).get();
-
-        //Voto
-        Voto voto = new Voto();
-        voto.setRonda(1);
-        voto.setTurno(1);
-        voto.setPartida(partida1);
-        votoService.saveVoto(voto,null);
-
-        List<Voto> votos = votoService.getVotosRondaTurno(partida1);
+    public void getVotosRondaTurnoTest() throws Exception{
+        Partida partida = partidaService.getPartidaById(1).orElse(null);
+        partida.setRonda(1);
+        partida.setTurno(1);
+        List<Voto> votos = votoService.getVotosRondaTurno(partida);
         assertFalse(votos.isEmpty());
         assertNotNull(votos);
     }
 
     @Test
     public void getVotosRondaTurnoFailTest() throws VotoNoPermitidoException{
-        //Partida
-        Partida partida1 = partidaService.getPartidaById(1).get();
-
-        //Voto
-        Voto voto = new Voto();
-        voto.setRonda(4); //Ronda que no existe
-        voto.setTurno(1);
-        voto.setPartida(partida1);
-        votoService.saveVoto(voto,null);
-
-        List<Voto> votos = votoService.getVotosRondaTurno(partida1);
+        Partida partida = partidaService.getPartidaById(1).orElse(null);
+        partida.setRonda(1);
+        partida.setTurno(2);
+        List<Voto> votos = votoService.getVotosRondaTurno(partida);
         assertTrue(votos.isEmpty());
-    }
-
-    @Test
-    public void saveVotoTest() throws VotoNoPermitidoException {
-        Voto voto = new Voto();
-        voto.setRonda(2);
-        voto.setTurno(1);
-        votoService.saveVoto(voto,null);
-
-        List<Voto> votos = votoService.getVotos();
-        assertEquals(voto, votos.get(votos.size()-1));
     }
     
     @Test
     public void getVotosTurnoJugadorTest() throws Exception{
-        //Partida
-        Partida partida1 = partidaService.getPartidaById(1).get();
-        Jugador jugador = jugadorService.getJugadorByUsername("Guaje");
-
-        //Voto
-        Voto voto = new Voto();
-        voto.setRonda(1);
-        voto.setTurno(1);
-        voto.setJugador(jugador);
-        voto.setPartida(partida1);
-        votoService.saveVoto(voto,null);
-
-        List<Voto> votos = votoService.getVotosTurnoJugador(partida1, jugador);
+        Partida partida = partidaService.getPartidaById(1).orElse(null);
+        partida.setRonda(1);
+        partida.setTurno(1);
+        Jugador jugador = jugadorService.getJugadorById(1).orElse(null);
+        List<Voto> votos = votoService.getVotosTurnoJugador(partida, jugador);
         assertFalse(votos.isEmpty());
         assertNotNull(votos);
     }
@@ -111,43 +106,25 @@ public class VotoServiceTests {
     @Test
     public void getVotosTurnoJugadorFailTest() throws Exception{
         //Partida
-        Partida partida1 = partidaService.getPartidaById(1).get();
-        Jugador jugador = jugadorService.getJugadorByUsername("JugadorPrueba");
-        Jugador jugador2 = jugadorService.getJugadorByUsername("Manolo");
-        Voto voto = new Voto();
+        Partida partida = partidaService.getPartidaById(1).orElse(null);
+        partida.setRonda(1);
+        partida.setTurno(1);
+        Jugador jugador = jugadorService.getJugadorById(1).orElse(null);
+        Jugador jugador2 = jugadorService.getJugadorById(6).orElse(null);
 
-        //Voto con jugador que no esta en la partida
-        voto.setRonda(1);
-        voto.setTurno(1);
-        voto.setJugador(jugador); //Jugador que no esta en la partida
-        voto.setPartida(partida1);
-        votoService.saveVoto(voto,null);
-
-        assertThrows(Exception.class,()->votoService.getVotosTurnoJugador(partida1, jugador));
-
+        assertThrows(Exception.class,()->votoService.getVotosTurnoJugador(partida, jugador2));
         //Voto con ronda que no existe
-        voto.setRonda(2);
-        voto.setTurno(1);
-        voto.setJugador(jugador2);
-        voto.setPartida(partida1);
-        votoService.saveVoto(voto,null);
-
-        List<Voto> votos = votoService.getVotosTurnoJugador(partida1, jugador2);
+        partida.setRonda(2);
+        partida.setTurno(2);
+        List<Voto> votos = votoService.getVotosTurnoJugador(partida, jugador);
         assertTrue(votos.isEmpty());
     }
 
     @Test
     public void getVotoByIdTest() throws VotoNoPermitidoException {
-        //Añadir voto
-        Voto voto = new Voto();
-        voto.setRonda(2);
-        voto.setTurno(1);
-        votoService.saveVoto(voto,null);
-
-        Voto voto2 = votoService.getVotoById((long) voto.getId()).orElse(null);
-        
+        Voto voto2 = votoService.getVotoById(1L).orElse(null);
         assertNotNull(voto2); 
-        assertEquals(voto, voto2);
+
     }
 
     @Test
@@ -155,4 +132,30 @@ public class VotoServiceTests {
         Voto voto = votoService.getVotoById((long) 5467834).orElse(null);
         assertNull(voto);
     }
+
+    @Test
+    public void saveVotoDeConsul(){
+        RolType rt = new RolType();
+        rt.setId(12341234);
+        rt.setName("Consul");
+        j.setRol(null);
+        assertThrows(VotoNoPermitidoException.class, ()->votoService.saveVoto(v, j));
+    }
+
+    @Test
+    public void saveVotoDeEdilRepetido(){
+        RolType rt = new RolType();
+        rt.setId(123123);
+        rt.setName("Edil");
+        j.setRol(rt);
+        try{
+            votoService.saveVoto(v, j);
+        }catch(Exception E){
+
+        }
+        v.setId(2324);
+        assertThrows(VotoNoPermitidoException.class, ()->votoService.saveVoto(v, j));
+    }
+
+
 }
